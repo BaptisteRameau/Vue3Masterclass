006413ce7a0a71064ad7fece27f787692e7e8bc0
Baptiste RAMEAU <rameaubaptiste@outlook.fr>
2023-04-28 13:23:09 +0200

    feat: install and implement vue-router

--- vue3-masterclass/src/App.vue
+++ vue3-masterclass/src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <PageHome/>
+  <RouterView/>
 </template>
 
 <script>
-import PageHome from '@/components/PageHome.vue'
-
 export default {
   name: 'App',
   components: {
-    PageHome
   }
 }
 </script>
 
 <style>
 @import "assets/style.css";
-@import "~bootstrap/css/bootstrap.css";
 </style>
