37db6cd5e5f2bc028954f8328de54a5dd3bb1324
Baptiste RAMEAU <rameaubaptiste@outlook.fr>
2023-05-10 09:01:18 +0200

    feat: form create post

--- vue3-masterclass/src/pages/ThreadShow.vue
+++ vue3-masterclass/src/pages/ThreadShow.vue
@@ -4,6 +4,17 @@
       <h1>{{ thread.title }}</h1>
 
       <PostList :posts="threadPosts" />
+
+      <div class="col-full">
+        <form @submit.prevent="addPost">
+            <div class="form-group">
+                <textarea v-model="newPostText" name="" id="" cols="30" rows="10" class="form-input"/>
+            </div>
+            <div class="form-actions">
+                <button class="btn-blue">Submit post</button>
+            </div>
+        </form>
+      </div>
     </div>
   </template>
   
@@ -27,6 +38,7 @@ export default {
         return {
             threads: sourceData.threads,
             posts: sourceData.posts,
+            newPostText: ''
         }
     },
 
@@ -39,5 +51,21 @@ export default {
             return this.posts.filter(post => post.threadId === this.id)
         }
     },
+
+    methods: {
+        addPost () {
+            const postId = 'ggqq' + Math.random()
+            const post = {
+                id: postId,
+                text: this.newPostText,
+                publishedAt: Math.floor(Date.now() / 1000),
+                threadId: this.id,
+                userId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+            }
+            this.posts.push(post)
+            this.thread.posts.push(postId)
+            this.newPostText = ''
+        }
+    }
 }
 </script>
